eaa8b142c659c8dbb9fd645f505aafc74dcc447c
Martin Masevski <martin.masevski@gmail.com>
2024-11-18 14:54:36 +0100

    disable overwritten inside custom component

--- app.vue
+++ app.vue
@@ -1,18 +1,49 @@
+<script setup lang="ts">
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const fakeSubmit = async (index) => {
+  console.log('Form submitted', index)
+  await sleep(3000)
+}
+</script>
+
 <template>
   <div class="m-4">
-    <FormKit
-      type="text"
-      label="Username"
-      :validation="[
-        ['required'],
-        ['length', 3, 20],
-        ['matches', /^[A-Za-z0-9\_]+$/],
-      ]"
-      validation-visibility="live"
-      :validation-messages="{
-        matches: 'Please enter a valid Username.',
-      }"
-      placeholder="Enter your Username..."
-    />
+    <FormKit type="form" @submit="fakeSubmit(1)" :actions="false">
+      <FormKit
+        type="text"
+        label="Username"
+        :validation="[['required']]"
+        validation-visibility="live"
+        :validation-messages="{
+          matches: 'Please enter a valid Username.',
+        }"
+        placeholder="Enter your Username..."
+      />
+    </FormKit>
+
+    <FormKit type="form" @submit="fakeSubmit(2)" :actions="false">
+      <MyTextInput
+        label="Username"
+        :validation="[['required']]"
+        validation-visibility="live"
+        :validation-messages="{
+          matches: 'Please enter a valid Username.',
+        }"
+        placeholder="Enter your Username..."
+      />
+    </FormKit>
+
+    <FormKit type="form" @submit="fakeSubmit(3)" :actions="false">
+      <MyTextInputBad
+        label="Username"
+        :validation="[['required']]"
+        validation-visibility="live"
+        :validation-messages="{
+          matches: 'Please enter a valid Username.',
+        }"
+        placeholder="Enter your Username..."
+      />
+    </FormKit>
   </div>
 </template>
---
+++ components/MyTextInputBad.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  disabled?: boolean
+}>()
+</script>
+
+<template>
+  <FormKit type="text" :disabled="disabled">
+    <template #label>CUSTOM LABEL</template>
+  </FormKit>
+</template>
